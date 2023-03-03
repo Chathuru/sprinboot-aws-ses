3ae52ab8e49c0a6b23a2c2cd1ed47c9571876ad2
chathura <chathurams.sa@gmail.com>
2023-03-03 15:46:16 +0000

    s3

--- src/main/java/com/chathuru/sesclient/ListObjects.java
+++ src/main/java/com/chathuru/sesclient/ListObjects.java
@@ -10,6 +10,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ListObjectsV2Result;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.auth.WebIdentityTokenCredentialsProvider;
 
 import java.util.List;
 
@@ -18,7 +19,8 @@ import java.util.List;
 public class ListObjects {
     @GetMapping(path="/")
     public @ResponseBody List<S3ObjectSummary> listObjects() {
-        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
+        //WebIdentityTokenCredentialsProvider awsCredentialProvider = WebIdentityTokenCredentialsProvider.builder().build();
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).withCredentials(new WebIdentityTokenCredentialsProvider()).build();
         ListObjectsV2Result result = s3.listObjectsV2("chathuru-java-sdk-test");
         List<S3ObjectSummary> objects = result.getObjectSummaries();
 
